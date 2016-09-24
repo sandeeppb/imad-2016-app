003656e05f6ce046859a417898f73f25e4d782c5
sandeeppb <sandeeppb@gmail.com>
2016-09-24 14:48:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/articleone', function (req, res) {
+  res.send('Article one requested will be served soon');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
