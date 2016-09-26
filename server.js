c1a75c968e8103212433b851bef4d3fb48bb6874
sandeeppb <sandeeppb@gmail.com>
2016-09-27 00:32:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-   res.send(createtemplate(articleone));
+   res.send(createtemplate(articles[articleone]));
 });
 app.get('/article-two', function (req, res) {
    res.send(createtemplate(articletwo));
