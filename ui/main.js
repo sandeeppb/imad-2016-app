6edc7d2b2a704406c5bfd7c686f845b8f5c6e7da
sandeeppb <sandeeppb@gmail.com>
2016-10-03 22:11:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,5 @@ console.log('Loaded!');
 var img = document.getElementById('img');
 img.onclick = function() {
      var i=60; 
-     img.style.marginleft='(i+10)px';
+     img.style.marginleft='(i+40)px';
 };
