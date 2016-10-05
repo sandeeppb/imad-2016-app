54e56adcc5cde61c69d82bb8ed47f8946dabe4fc
sandeeppb <sandeeppb@gmail.com>
2016-10-06 00:12:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
-var button = document.get
+button=document.getElementById('counter');
+button.onclick = function() 
+{
+    var request=new XMLHttpRequest();
+    request.onreadystatechange = function() 
+    {
+        if (request.readyState===XMLHttpRequest.DONE)
+        {if(request.status===200)
+            {var counter= request.responseText;
+             var span = document.getElementById('count');
+             span.innerHTML=counter.toString();
+            }
+        }
+    };
+};
