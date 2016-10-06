89bfec1e4e7608cbc73dfc969d09bc9021ad898e
sandeeppb <sandeeppb@gmail.com>
2016-10-06 10:28:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,6 +12,6 @@ button.onclick = function()
             }
         }
     };
-request.open('GET','http://http://sandeeppb.imad.hasura-app.io/counter',true);
+request.open('GET','http://sandeeppb.imad.hasura-app.io/counter',true);
 request.send(null);
 };
